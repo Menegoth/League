ffac0652b363e122551dff457ac233c7591b7040
Antonio Monteiro <antonioareas@me.com>
2019-03-29 20:24:04 -0700

    Added additional error messages

--- GamesPlayed.py
+++ GamesPlayed.py
@@ -14,15 +14,23 @@ parameters = {"api_key": apiKey}
 response = requests.get(endpoints["accountData"], params=parameters)
 
 if response.status_code == 200:
+
     accountData = json.loads(response.content)
     accountID = accountData["accountId"]
 
     endpoints["matchesData"] = f"{url}/match/v4/matchlists/by-account/{accountID}"
 
     response = requests.get(endpoints["matchesData"], params=parameters)
+
     if response.status_code == 200:
         matchesData = json.loads(response.content)
 
         print(matchesData["totalGames"])
-else:
-    print("Something went wrong")
+
+    elif response.status_code == 401:
+
+        print("Authentication failed.")
+
+elif response.status_code == 401:
+    
+    print("Authentication failed.")
